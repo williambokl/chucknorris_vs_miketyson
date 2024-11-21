acb3824e8fa9b6984bc05d23f9b3209e9fda1197
William Bitencourt <williambokl@gmail.com>
2024-11-20 21:21:11 -0300

    efeitos

--- index.js
+++ index.js
@@ -19,10 +19,15 @@ async function getData() {
       const mensagemElement = document.getElementById("mensagem");
       const lutaElement = document.getElementById("luta");
       const perdeuElement = document.getElementById("perdeu");
+      const venceuElement = document.getElementById("vencedor");
+      const buttonElement = document.getElementById("botao");
      
+      venceuElement.textContent = "Congratulations, you defeated Mike Tyson! As a reward for your epic vitory, here's a legendary Chuck Norris quote to inspire you";
       lutaElement.style.display = "none";
-      perdeuElement.style.display = "inline"
+      perdeuElement.style.display = "inline";
+      buttonElement.style.display = "none";
       mensagemElement.innerText = json.value;
+
 
     } catch (error) {
       console.error(error.message);
@@ -32,7 +37,52 @@ async function getData() {
 var acao =  document.querySelector('.acao');
 
 acao.addEventListener("click", (e) => {
-    getData();
+  confetti({
+    particleCount: 100,
+    spread: 70,
+    origin: { y: 0.6 },
+  });
+  
+  const count = 200,
+    defaults = {
+      origin: { y: 0.7 },
+    };
+
+  function fire(particleRatio, opts) {
+    confetti(
+      Object.assign({}, defaults, opts, {
+        particleCount: Math.floor(count * particleRatio),
+      })
+    );
+  }
+  fire(0.25, {
+    spread: 26,
+    startVelocity: 55,
+  });
+  
+  fire(0.2, {
+    spread: 60,
+  });
+  
+  fire(0.35, {
+    spread: 100,
+    decay: 0.91,
+    scalar: 0.8,
+  });
+  
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 25,
+    decay: 0.92,
+    scalar: 1.2,
+  });
+  
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 45,
+  });
+  getData();
+
 
 })
 
